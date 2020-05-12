fba74d10b358e3976aa465d54c80baa5a398b10f
sofiabhutia <45333218+sofiabhutia@users.noreply.github.com>
2020-05-12 13:07:37 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,14 +1,14 @@
 
 
 // changing individual properties with code and using setInterval
-var rotationSpeed = 0.05;
+var rotationSpeed = 0.001;
  var myOtherBox = document.getElementById('myTorusKnot');
 
  function spin(){
  	myTorusKnot.object3D.rotation.x -= rotationSpeed;
  	myTorusKnot.object3D.rotation.y -= rotationSpeed;
  	//myTorusKnot.object3D.rotation.z -= rotationSpeed;
- 	console.log(myTorusKnot.object3D.rotation.x);
+ 	console.log(rotationSpeed);
  }
 
  setInterval(spin, 16); //equivalent to 60 fps
