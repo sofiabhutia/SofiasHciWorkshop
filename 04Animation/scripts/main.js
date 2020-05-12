78a54419ca41542f1f2fd09cf796a7166a1c5723
sofiabhutia <45333218+sofiabhutia@users.noreply.github.com>
2020-05-12 11:37:24 +0200

    spinnies

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -6,6 +6,8 @@ var rotationSpeed = 0.01;
 
  function spin(){
  	myOtherBox.object3D.rotation.x -= rotationSpeed;
+ 	myOtherBox.object3D.rotation.y -= rotationSpeed;
+ 	myOtherBox.object3D.rotation.z -= rotationSpeed;
  	console.log(myOtherBox.object3D.rotation.x);
  }
 
