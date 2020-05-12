54b76e0b3dbb25c569357d6e82bfb672ac0f16a3
sofiabhutia <45333218+sofiabhutia@users.noreply.github.com>
2020-05-12 12:48:07 +0200

    torus

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,8 +1,8 @@
 
 
 // changing individual properties with code and using setInterval
-var rotationSpeed = 0.1;
- var myOtherBox = document.getElementById('myOtherBox');
+var rotationSpeed = 0.05;
+ var myOtherBox = document.getElementById('myTorusKnot');
 
  function spin(){
  	myOtherBox.object3D.rotation.x -= rotationSpeed;
